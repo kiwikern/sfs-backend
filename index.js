fe4480a88052106b5e3855ae7c23ae2e838b10a8
Kim Kern <kim.kern@fu-berlin.de>
2017-03-16 00:03:51 +0100

    finish parsing website and persist to file

--- index.js
+++ index.js
@@ -2,25 +2,57 @@ const jsdom = require("jsdom");
 const fetch = require("node-fetch");
 const config = require("./config.js");
 const jQuery = require('jquery');
+const fs = require('fs');
 
+const scheduleJSON = getCourseInfosForAllStudios()
+  .then(json => persist(json));
 
-const studioPath = config.studios[0];
-const studioUrl = config.baseUrl + '/' + studioPath;
-const dayPath = config.days.tuesday;
-let dayUrl = studioUrl
-if (dayPath.length > 0) {
-  dayUrl = dayUrl + '/' + dayPath;
+function persist(scheduleJSON) {
+  fs.writeFile("./schedule.json", scheduleJSON, err => err ? console.log(err) : console.log('file saved'));
 }
 
-getCourseInfosForOneDay(dayUrl);
+function getCourseInfosForAllStudios() {
+  let schedule = {};
+  let promises = [];
+  for (let studio of config.studios) {
+    const studioUrl = config.baseUrl + '/' + studio;
+    let promise = getCourseInfoForAllDays(studioUrl)
+    .then(courses => schedule[studio] = courses)
+    .catch(error => console.log(error));
+    promises.push(promise);
+  }
+  return Promise.all(promises)
+  .then(() => JSON.stringify(schedule, null, 2));
+}
+
+function getCourseInfoForAllDays(studioUrl) {
+  let result = {};
+  let promises = [];
+  for (let day in config.days) {
+    let dayPath = config.days[day];
+    let dayUrl;
+    if (dayPath.length > 0) {
+      dayUrl = studioUrl + '/' + dayPath;
+    } else {
+      dayUrl = studioUrl;
+    }
+    let promise = getCourseInfosForOneDay(dayUrl)
+      .then(courses => result[day] = courses);
+    promises.push(promise);
+  }
+  return Promise.all(promises)
+    .then(() => result);
+}
+
+
 
 function getCourseInfosForOneDay(url) {
-  fetch(url)
+  return fetch(url)
     .then(res => res.text())
-    .then(parse);
+    .then(parseWebsite);
 }
 
-function parse(body) {
+function parseWebsite(body) {
   return new Promise(resolve => {
     jsdom.env(
       body,
@@ -28,7 +60,6 @@ function parse(body) {
         let $ = jQuery(window);
         let tables = $('#content table');
         let courseInfos = $.map(tables, t => getCourseInfo($(t)));
-        console.dir(courseInfos)
         resolve(courseInfos);
       });
     });
