d7ceeabd5d992a3240dedcec185ece029bf935a8
Kim Kern <github@kimkern.de>
2017-05-18 14:56:07 +0200

    add cron job for reloading schedule every night at 4am

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const coursesParser = require('./courses-parser.js');
 const scheduleReader = require('./schedule-reader.js');
+const cron = require('node-cron');
 const route = require('koa-route');
 const koa = require('koa');
 const app = new koa();
@@ -15,6 +16,11 @@ app.use(route.get('/schedule', ctx => {
 app.use(route.get('/reload', reload));
 
 app.listen(3000);
+
+/**
+ * Reload schedule every day at 4 am.
+ */
+cron.schedule('0 4 * * *', reload);
 
 function reload() {
   coursesParser.parseCourses()
