dd555b35ac282dd2bb7a93da9d43c022e4a6184c
Kim Kern <kim.kern@fu-berlin.de>
2017-05-18 12:24:15 +0200

    add config.js to repository, not ignored

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = module.exports = {};
+config.baseUrl = 'http://m.mysuperfit.com/kursplaene';
+config.studios = ['berlin-europa-center', 'berlin-europa-center-cycle',
+'berlin-charlottenburg', 'berlin-friedrichshain', 'berlin-koepenick',
+'berlin-mitte', 'berlin-steglitz', 'berlin-steglitz-women', 'berlin-tegel',
+'brandenburg-potsdam'];
+config.days = {monday : '', tuesday: 'di', wednesday: 'mi', thursday: 'do',
+  friday: 'fr', saturday: 'sa', sunday: 'so'};
