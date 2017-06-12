ebca3427a9197e75dfcdb92d756be920fffbcec7
Kim Kern <github@kimkern.de>
2017-06-12 21:13:19 +0200

    change: jwt is valid for 30 days

--- src/user/token.generator.js
+++ src/user/token.generator.js
@@ -4,7 +4,7 @@ const jwtSecret = require('../secrets.js').jwt;
 exports.generateToken = (user, ctx) => {
   const content = {id: user._id, username: user.userName};
   try {
-    const jwtOptions = {algorithm: 'HS256', expiresIn: '1h'};
+    const jwtOptions = {algorithm: 'HS256', expiresIn: '30d'};
     const token =  jwt.sign(content, jwtSecret.privateKey, jwtOptions);
     ctx.response.body = {token, userName: user.userName};
     ctx.response.status = 201;
