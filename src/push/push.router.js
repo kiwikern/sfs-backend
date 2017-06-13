535a32c832da77ebe863566385d14521da134b51
Kim Kern <github@kimkern.de>
2017-06-13 23:19:59 +0200

    remove auth from push routes

--- src/push/push.router.js
+++ src/push/push.router.js
@@ -1,6 +1,4 @@
 const router = require('koa-router')();
-const jwt = require('koa-jwt');
-const jwtSecret = require('../secrets.js').jwt;
 const pushSaver = require('./push.saver.js');
 const pushSender = require('./push.sender.js');
 
@@ -8,7 +6,4 @@ exports.routes = () => router.routes();
 
 pushSender.init();
 router.post('/', (ctx) => pushSaver.saveSubscription(ctx));
-
-// protected routes
-// router.use(jwt({secret: jwtSecret.privateKey}));
 router.get('/send', pushSender.sendPush);
